a35e81d5502f7507dd8731da1ecbae0fc069bfb8
minjaeleee <dlalswo4602@naver.com>
2023-02-18 22:19:05 +0900

    react-dropzone을 사용한 이미지 드래그 기능 추가 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
-import React, { useRef, useState } from 'react';
+import {useDropzone} from 'react-dropzone'
+import React, { useRef, useState, useCallback } from 'react';
 import './App.css';
 import ImageBox from './components/ImageBox';
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null)
-
   const [imaegList, setImageList] = useState<string[]>([])
 
+  const onDrop = useCallback((acceptedFiles:any) => {
+    for(const file of acceptedFiles){
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onloadend = event => {
+        setImageList(prev => [...prev, event.target?.result as string])
+      }  
+    }
+  }, [])
+
+  const {getRootProps, getInputProps, open} = useDropzone({onDrop})
   return (
     <div className='container'>
-      <div className={`wrapper_box ${imaegList.length > 0 ? 'row' : '' }`}>
-        {
-          imaegList.length === 0 &&
-          <div className='text'>
-            이미지가 없습니다. <br />
-            이미지를 추가해주세요.
-          </div>
-        }
-        <input className='input_image' type='file' ref={inputRef} onChange={(event)=>{
-          if(event.currentTarget.files?.[0]) {
-            const reader = new FileReader()
-            const file = event.currentTarget.files?.[0];
-            reader.readAsDataURL(file)
-            reader.onloadend = event => {
-                setImageList(prev => [...prev, event.target?.result as string])
-            }
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <div className={`wrapper_box ${imaegList.length > 0 ? 'row' : '' }`}>
+          {
+            imaegList.length === 0 &&
+            <div className='text'>
+              이미지가 없습니다. <br />
+              이미지를 추가해주세요.
+            </div>
           }
-        }} />
-        {
-          imaegList.map(img => <ImageBox key={img} src={img}/>)
-        }
-        <div className='plus_box' onClick={()=>{
-          inputRef.current?.click()
-        }}>
-          +
+              {
+                imaegList.map((img,idx) => <ImageBox key={img+idx} src={img}/>)
+              }
+            <div className='plus_box' onClick={open}>
+              <input className='input_image' type='file' ref={inputRef} />
+                    +
+                </div>
+            </div>    
         </div>
-      </div>
     </div>
   );
 }
